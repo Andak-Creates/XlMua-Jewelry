3fc717e1d234407a7535cf19e0e2b6055393d5a3
Vindi <kelvinandak@gmail.com>
2024-03-11 02:01:22 +0100

    started Xcel mua jewelry, added all images

---
+++ src/Components/Tools.jsx
@@ -0,0 +1,26 @@
+// export const myJewel = [{}];
+
+// export const myWatches = [
+//   {
+//     smartX8: [
+//       // smart x8
+//       {
+//         img: "smartWatch8x.jpg",
+//         name: "SMART X8 PLUS PACKAGE",
+//         consistOf: [
+//           "MAGNETIC POWER BANK",
+//           "EARPOD",
+//           "SMART CABLE CHARGER",
+//           "SMART CORD",
+//           " DUAL SIM CELL PHONE which has ABILITIES to ( CALLS, MESSAGES , PHONEBOOK, SERVICES , BLUETOOTH DIALER ) and much more",
+//         ],
+//       },
+//     ],
+//   },
+
+//   //   DW watch for men
+//   {
+//     img: "dwForMen.jpg",
+//     name: "DW watch for men",
+//   },
+// ];
